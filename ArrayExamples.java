52c196c3ca3b9b193c02871697672a157aae8664
benly03 <156245119+benly03@users.noreply.github.com>
2024-01-31 13:06:06 -0800

    Fixed reverse methods

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,18 +4,21 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+    for(int i = arr.length / 2; i < arr.length; i += 1) {
+      int temp = arr[arr.length - i - 1]; // stores last element in array as temp
+      arr[i] = arr[arr.length - i - 1]; // puts last element at the front of the array
+      arr[arr.length - i - 1] = temp;
     }
   }
 
   // Returns a *new* array with all the elements of the input array in reversed
   // order
   static int[] reversed(int[] arr) {
-    int[] newArray = new int[arr.length];
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+    int[] newArray = new int[arr.length]; // creates new array w/ original array length
+    for(int i = 0; i < arr.length; i += 1) { // iterates through every element in the array
+      newArray[arr.length - i - 1] = arr[i];
     }
+    arr = newArray;
     return arr;
   }
 
@@ -23,12 +26,12 @@ public class ArrayExamples {
   // lowest number when calculating. Returns 0 if there are no elements or just
   // 1 element in the array
   static double averageWithoutLowest(double[] arr) {
-    if(arr.length < 2) { return 0.0; }
-    double lowest = arr[0];
-    for(double num: arr) {
+    if(arr.length < 2) { return 0.0; } // check is length is less than 2, if so return 0
+    double lowest = arr[0]; // set first element to be the lowest
+    for(double num: arr) { // iterate through every element in the array
       if(num < lowest) { lowest = num; }
     }
-    double sum = 0;
+    double sum = 0.0;
     for(double num: arr) {
       if(num != lowest) { sum += num; }
     }
--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,30 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace1() {
+    int[] input2 = {1, 2};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{2, 1}, input2);
+  }
+
+  @Test
+  public void testReverseInPlace2() {
+    int[] input2 = {1, 2, 3, 3};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{3, 3, 2, 1}, input2);
+  }
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = {1, 2};
+    assertArrayEquals(new int[]{2, 1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = {1, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input1));
+  }
 }
